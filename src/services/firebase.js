0cb35b12429b993cc1cc9a2eebd141d483ed7aee
Bhoomi Singh <bhoomisingh7988@gmail.com>
2026-01-07 21:21:09 +0530

    update#

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -16,13 +16,14 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD7eaDyXialcBdTwnuykERoymZX6xVULfA",
-  authDomain: "signage-4c716.firebaseapp.com",
-  projectId: "signage-4c716",
-  storageBucket: "signage-4c716.firebasestorage.app",
-  messagingSenderId: "327788004598",
-  appId: "1:327788004598:web:46ad33a697a31fe75858d7",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
